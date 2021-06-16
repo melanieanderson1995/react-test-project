cbfc42839ed86160ed2ffb6c46e329e12fd945d9
Melanie Anderson <melanieanderson1995@gmail.com>
2021-06-16 08:05:29 -0400

    Changed to grid list, list now displays, added search functionality, no design currently implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,149 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/header.js";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  CardMedia,
+  Card,
+  GridList,
+  Typography,
+  SvgIcon,
+} from "@material-ui/core";
+import React, { Component, useEffect } from "react";
+import Header from "../src/components/header";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import SearchBar from "material-ui-search-bar";
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
+const styles = (theme) => ({
+  root: {
+    backgroundColor: "red",
+  },
+});
+
+class App extends Component {
+  state = {
+    loading: true,
+    items: [],
+    filter: "",
+  };
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=24&nat=us")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          items: response.results,
+          loading: false,
+        });
+      });
+  }
+
+  filterSearch() {
+    return this.state.items.filter((item) => {
+      const fullname = item.name.first + item.name.last;
+      return fullname.toLowerCase().includes(this.state.filter.toLowerCase());
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div style={{ backgroundColor: "blue" }}>
+        <div>
+          <Header></Header>
+        </div>
+        {this.state.loading && (
+          <div>
+            <Typography style={{ fontSize: 30 }}>Loading...</Typography>
+          </div>
+        )}
+        {!this.state.loading && (
+          <div
+            style={{
+              margin: 30,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <SearchBar
+              style={{ margin: 30, maxWidth: 300 }}
+              value={this.state.filter}
+              onChange={(newFilter) => this.setState({ filter: newFilter })}
+              onRequestSearch={() => {
+                this.filterSearch();
+              }}
+            ></SearchBar>
+            <GridList
+              className={classes.root}
+              cellHeight="auto"
+              cols={5}
+              spacing={5}
+              style={{ alignItems: "center", justifyContent: "center" }}
+            >
+              {this.state.filter
+                ? this.filterSearch().map((item) => (
+                    <Card
+                      style={{
+                        flexDirection: "row",
+                        padding: 20,
+                        margin: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div>
+                        <Avatar style={{ width: 100, height: 100 }}>
+                          <img src={item.picture.large} />
+                        </Avatar>
+                      </div>
+                      <div style={{ flexDirection: "column", paddingLeft: 10 }}>
+                        <Typography style={{ fontSize: 20 }}>
+                          {item.name.first} {item.name.last}
+                        </Typography>
+                        <Typography style={{ fontSize: 12 }}>
+                          {item.location.city}, {item.location.state}
+                        </Typography>
+                        <Typography style={{ fontSize: 12 }}>
+                          {item.email}
+                        </Typography>
+                      </div>
+                    </Card>
+                  ))
+                : this.state.items.map((item) => (
+                    <Card
+                      style={{
+                        flexDirection: "row",
+                        padding: 20,
+                        margin: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div>
+                        <Avatar style={{ width: 100, height: 100 }}>
+                          <img src={item.picture.large} />
+                        </Avatar>
+                      </div>
+                      <div style={{ flexDirection: "column", paddingLeft: 10 }}>
+                        <Typography style={{ fontSize: 20 }}>
+                          {item.name.first} {item.name.last}
+                        </Typography>
+                        <Typography style={{ fontSize: 12 }}>
+                          {item.location.city}, {item.location.state}
+                        </Typography>
+                        <Typography style={{ fontSize: 12 }}>
+                          {item.email}
+                        </Typography>
+                      </div>
+                    </Card>
+                  ))}
+            </GridList>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withStyles(styles)(App);
--- src/components/header.js
+++ src/components/header.js
@@ -1,78 +1,88 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  makeStyles,
-  Button,
-} from "@material-ui/core";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
-const useStyles = makeStyles(() => ({
-  header: {
-    backgroundColor: "grey",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
   },
 }));
 
-const headersData = [
-  {
-    label: "Employees",
-  },
-  { label: "Departments" },
-  { label: "Org Chart" },
-  { label: "Locations" },
-  { label: "Teams" },
-];
-
-export default function Header() {
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
-    <AppBar position="fixed">
-      <Toolbar style={header}>
-        <div
-          style={{
-            flex: 1,
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-            backgroundColor: "red",
-          }}
-        >
-          <Typography variant="h6">Employee Directory</Typography>
-        </div>
-        <div
-          style={{
-            alignItems: "flex-end",
-            justifyContent: "flex-end",
-          }}
-        >
-          <Button color="inherit" onClick={handleClick}>
-            Open Menu
-          </Button>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Employee Directory
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenu}
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+          >
+            <MenuIcon />
+          </IconButton>
           <Menu
-            id="simple-menu"
+            id="menu-appbar"
             anchorEl={anchorEl}
-            keepmounted
-            open={Boolean(anchorEl)}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={open}
             onClose={handleClose}
           >
             <MenuItem onClick={handleClose}>Employees</MenuItem>
             <MenuItem onClick={handleClose}>Departments</MenuItem>
-            <MenuItem onClick={handleClose}>Org Chart</MenuItem>
+            <MenuItem onClick={handleClose}>Org Chat</MenuItem>
             <MenuItem onClick={handleClose}>Locations</MenuItem>
             <MenuItem onClick={handleClose}>Teams</MenuItem>
           </Menu>
-        </div>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
